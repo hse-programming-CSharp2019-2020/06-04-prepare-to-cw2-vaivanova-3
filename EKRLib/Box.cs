7172699d80f586b24d956ea8e901821452d2aee7
Виолетта Иванова <vaivanova_3@edu.hse.ru>
2020-06-03 17:18:24 +0300

    Добавьте файлы проекта.

---
+++ EKRLib/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKRLib
+{
+    class Box : Item
+    {
+        private double A
+        {
+            set;
+            get;
+        }
+        private double B
+        {
+            set;
+            get;
+        }
+        private double C
+        {
+            set;
+            get;
+        }
+
+        double GetLongestSideSize()
+        {
+            return Math.Max(Math.Max(A, B), C);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"A: {A}, B: {B}, C: {C}.";
+        }
+    }
+}
---
+++ EKRLib/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKRLib
+{
+    public class Item : IComparable<Item>
+    {
+
+        private double Weight
+        {
+            set;
+            get;
+        }
+
+        public int CompareTo(Item obj)
+        {
+            return (this.Weight).CompareTo(obj.Weight);
+        }
+
+        public static explicit operator double(Item obj)
+        {
+            return obj.Weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Weight: {Weight}.";
+        }
+    }
+}
